841b6fca5f8b76b2b92ae70ca3270f9457f4f1a8
Alanthadathil <alanthadathil73@gmail.com>
2023-09-23 21:48:21 +0530

    Website portfolio onprogress

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
 
-function page() {
-    return <div className={styles.container}>Portfolio</div>;
+function Portfolio() {
+    return (
+        <div className={styles.container}>
+            <div className="selectTitle">Choose a gallery</div>
+            <div className="items">
+                <Link href="/portfolio/illustrations" className="item">
+                    <span className="title"> illustrations</span>
+                </Link>
+                <Link href="/portfolio/websites" className="item">
+                    <span className="title"> illustrations</span>
+                </Link>
+                <Link href="/portfolio/applications" className="item">
+                    <span className="title"> illustrations</span>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
-export default page;
+export default Portfolio;
